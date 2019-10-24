85998d3069e2fd0d1d0f428207c0f50761c57e01
npkfrolov <npkfrolov@gmail.com>
2019-10-24 19:39:16 +0300

    пятый урок. задание 6

--- lesson5/homework5_6.py
+++ lesson5/homework5_6.py
@@ -5,3 +5,19 @@
 Сформировать словарь, содержащий название предмета и общее количество
 занятий по нему. Вывести словарь на экран."""
 
+with open("homework6.txt") as content:
+    my_dict = {}
+    for line in content:
+        my_key = line.split()[0]
+        total = 0
+        for el in line.split():
+            """здесь я схитрил - не придумал, как использовать 
+            несколько вариантов для разбивки на элементы (" ", ":", ", "). 
+        Поэтому перед двоеточием и запятой тоже поставил пробелы"""
+            try:
+                total = total + int(el)
+            except ValueError:
+                continue
+        my_dict.update({my_key: total})
+    print(my_dict)
+
