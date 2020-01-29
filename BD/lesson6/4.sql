dce83779385d67a3c92fae3eedc23f7ba43e08f4
Alexey Frolov <npkfrolov@gmail.com>
2020-01-29 08:08:04 +0300

    исправление в шестом уроке

--- BD/lesson6/4.sql
+++ BD/lesson6/4.sql
@@ -1,5 +1,5 @@
 SELECT CONCAT ('Лайков больше всего поставили', ' ', IF (
-			(SELECT SUM(id) as amount FROM likes WHERE user_id IN (
+			(SELECT COUNT(id) as amount FROM likes WHERE user_id IN (
 					SELECT user_id FROM profiles WHERE sex = 'f')) > 
-			(SELECT SUM(id) as amount FROM likes WHERE user_id IN (
-					SELECT user_id FROM profiles WHERE sex = 'm')), 'женщины', 'мужчины'), '') as result; 
+			(SELECT COUNT(id) as amount FROM likes WHERE user_id IN (
+					SELECT user_id FROM profiles WHERE sex = 'm')), 'женщины', 'мужчины')) as result; 
