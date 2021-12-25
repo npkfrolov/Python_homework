8da4cd1d6f1379841100645de078e2e69a024636
npkfrolov <npkfrolov@gmai.com>
2021-12-25 19:26:23 +0300

    Lesson3

--- experiment/lesson2/task2.py
+++ experiment/lesson2/task2.py
@@ -2,18 +2,24 @@
 
 import json
 
-def write_order_to_json(item, quantity, price, buyer, date):
-    with open('orders.json', 'w') as f_n:
-        dict_to_json = {
-            "item": item,
-            "quantity": quantity,
-            "price": price,
-            "buyer": buyer,
-            "date": date,
-        }
-        json.dump(dict_to_json, f_n, indent=4)
 
-    with open('orders.json') as f_n:
-        print(f_n.read())
+def write_order_to_json(item, quantity, price, buyer, date):
+
+    with open('orders.json', 'r') as file:
+        orders_data = json.load(file)
+
+    if not 'orders' in orders_data:
+        orders_data['orders'] = []
+    orders_data['orders'].append({
+        "item": item,
+        "quantity": quantity,
+        "price": price,
+        "buyer": buyer,
+        "date": date
+    })
+
+    with open('orders.json', 'w') as add_json:
+        json.dump(orders_data, add_json, indent=4)
+
 
 write_order_to_json("Товар", 3, 100, "Рога и копыта лимитед", "2021-12-22")
